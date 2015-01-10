9d6cead9282157d2486673147d2cbf14de115f2e
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-10 12:53:24 -0500

    Test for gravatars added

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,13 +1,11 @@
 module AvatarHelper
   def avatar
     if !current_user.avatar.url.nil?
-      image_tag current_user.avatar,
-      size: "150",
+      image_tag current_user.avatar.url,
       alt: "#{current_user.username}'s Avatar"
     else
       image_tag current_user.gravatar_url,
-      size: "150",
-      alt: "#{current_user.username}'s Avatar"
+      alt: "#{current_user.username}'s Gravatar"
     end
   end
 end
