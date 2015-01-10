213f5ae95aa9ee3e68fca7a5c0d85b34a0284e04
Daniel Bonnell <daniel.bonnell@gmail.com>
2015-01-10 12:09:59 -0500

    Fixed Hound errors for styling

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -2,12 +2,12 @@ module AvatarHelper
   def avatar
     if !current_user.avatar.url.nil?
       image_tag current_user.avatar,
-        size: "150", 
-        alt: "#{current_user.username}'s Avatar"
+      size: "150",
+      alt: "#{current_user.username}'s Avatar"
     else
       image_tag current_user.gravatar_url,
-        size: "150",
-        alt: "#{current_user.username}'s Avatar"
+      size: "150",
+      alt: "#{current_user.username}'s Avatar"
     end
   end
 end
